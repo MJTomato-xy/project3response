e5e7796c4c2b6b72e6cdde29a21e64bdc1499f4e
Xinyi-Liu-024 <59549169+Xinyi-Liu-024@users.noreply.github.com>
2020-01-27 14:37:01 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+function showResponse() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+  }
+
+  function showResponse1() {
+    var popup = document.getElementById("second");
+    popup.classList.toggle("show");
+  }
+
+  function showResponse2() {
+    var popup = document.getElementById("third");
+    popup.classList.toggle("show");
+  }
+
+  function showResponse3() {
+    var popup = document.getElementById("forth");
+    popup.classList.toggle("show");
+  }
+
+  function showResponse4() {
+    var popup = document.getElementById("fifth");
+    popup.classList.toggle("show");
+  }
+
+  function showResponse5() {
+    var popup = document.getElementById("sixth");
+    popup.classList.toggle("show");
+  }
+
+  function showResponse6() {
+    var popup = document.getElementById("seventh");
+    popup.classList.toggle("show");
+  }
